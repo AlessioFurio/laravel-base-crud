9b094fc8519993b54025d508eafb46589c8c50b3
Furio Alessio <alefurio@hotmail.it>
2021-01-20 16:24:51 +0100

    controller + link (home + products)

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@include('partials.header')
+@section('content')
+    <h1>Ciao</h1>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,8 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="{{ route('home') }}">Home</a></li>
+            <li><a href="{{ route('products.index') }}">Products</a></li>
+        </ul>
+    </nav>
+</header>
